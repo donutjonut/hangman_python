0fda80cb177465d267a0c3e758f0a1963a818fa7
donutjonut <donutjonut@gmail.com>
2021-12-14 20:44:06 -0600

    gamemer moment

--- game.py
+++ game.py
@@ -1,2 +1,24 @@
-def testUWU():
-    print("test")
+import requests
+import random
+
+def generateWord(): #generate a random word and return it
+    word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+    response = requests.get(word_site)
+    WORDS = response.content.splitlines()
+    randomWord = random.choice(WORDS).decode('utf-8')
+    return randomWord
+
+def runGame(randomWord, word, blank):
+    deathCheck = 0
+    ranCheck = False
+    while(deathCheck < 6):
+        check = input("What is your letter?").lower()
+        for j in range(len(randomWord)):
+            if (check == word[j]):
+                blank[j] = check
+                ranCheck = True
+        if(ranCheck == False):
+            deathCheck = deathCheck + 1
+        ranCheck = False
+        print(*blank, sep=" ")    
+        print("Death Check: " + str(deathCheck))
